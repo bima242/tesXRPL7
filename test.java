71a03e23de596eac5a13f15c16218c7c97675e71
bima242 <arya_tantular_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:00:07 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+         System.out.println("Nama saya Arya Bima Tantular dari kelas XR7 yang berasal dari kota Banyuwangi");    
+         String a = "1";
+    }
+    }
